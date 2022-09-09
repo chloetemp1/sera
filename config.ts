c8d7eb53897cdc798e28ae1b84e372dd99dd181b
Chloe Brett <cbre0013@student.monash.edu>
2022-09-09 17:15:03 +1000

    updarte uri

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
 export const CLIENT_ID = 'Iv1.f3143324d981069e';
-export const REDIRECT_URI = 'https://persona-pied-ten.vercel.app/api/githubLogin';
+export const REDIRECT_URI = 'https://persona-pied-ten.vercel.app/GithubLogin';
