19df1e072f72e9c76f922e16ec47f4886f5bd609
Chloe Brett <cbre0013@student.monash.edu>
2022-10-14 18:33:17 +1100

    wait to redirect

--- pages/api/githubLogin.ts
+++ pages/api/githubLogin.ts
@@ -8,6 +8,8 @@ export default async function handler(
 ) {
   const { code } = request.query;
 
+  console.log(request.query);
+
   try {
     const body = {
       code,
@@ -16,11 +18,15 @@ export default async function handler(
       redirect_uri: REDIRECT_URI,
     };
 
+    console.log(body);
+
     const { data } = await axios.post(
       "https://github.com/login/oauth/access_token",
       body,
       { headers: { Accept: "application/json" } }
     );
+
+    console.log(data);
 
     response.status(200).json({
       data,
